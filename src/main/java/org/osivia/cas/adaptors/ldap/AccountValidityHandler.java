d1eb7e9249cfbaf1ab30bf2aa161ce98191c0ac5
loic <loic@bebop>
2019-04-16 17:33:26 +0200

    #1978 Search by mail and bind + check validation date

--- src/main/java/org/osivia/cas/adaptors/ldap/AccountValidityHandler.java
+++ src/main/java/org/osivia/cas/adaptors/ldap/AccountValidityHandler.java
@@ -31,40 +31,25 @@ public class AccountValidityHandler extends LdapAuthenticationHandler {
         HandlerResult result = super.authenticateUsernamePasswordInternal(credentials);
         
         Map<String, Object> attributes = result.getPrincipal().getAttributes();
-        Object portalPersonExternal = attributes.get("portalPersonExternal");
-        
-        Boolean external = null;
-        if(portalPersonExternal != null) {
-        	
-        	external = BooleanUtils.toBooleanObject(portalPersonExternal.toString());
-        	
-        	// if portalPersonExternal = TRUE, must use FIM
-        	if(external) {
-        		throw new FailedLoginException("Invalid account (external)");
-        	}
-        	else {
-                Object portalPersonValidity = attributes.get("portalPersonValidity");
+            	
+        Object portalPersonValidity = attributes.get("portalPersonValidity");
 
-            	// if portalPersonExternal = FALSE, portalPersonValidity should be after current day
-                if(portalPersonValidity != null) {
-                	try {
-						String dayOfValidity = portalPersonValidity.toString().substring(0, 8);
-						Date validity = DateUtils.parseDate(dayOfValidity, "yyyyMMdd");
-						
-						if(validity.before(new Date())) {
-			        		throw new FailedLoginException("Account has expired");
-						}
-						
-					} catch (ParseException e) {
-		        		throw new FailedLoginException("Invalid expiration date");
+    	// if portalPersonExternal = FALSE, portalPersonValidity should be after current day
+        if(portalPersonValidity != null) {
+        	try {
+				String dayOfValidity = portalPersonValidity.toString().substring(0, 8);
+				Date validity = DateUtils.parseDate(dayOfValidity, "yyyyMMdd");
+				
+				if(validity.before(new Date())) {
+	        		throw new FailedLoginException("Account has expired");
+				}
+				
+			} catch (ParseException e) {
+        		throw new FailedLoginException("Invalid expiration date");
 
-					}
-                }
-
-        	}
-        	
+			}
         }
-        
+
         
         return result;
 	}
